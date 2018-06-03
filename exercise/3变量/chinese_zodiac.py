eb945f7b947dfc84c4e023850f167a4fe758efba
Zhao Shujian <shujianzhao@gmail.com>
2018-06-03 23:18:33 +0800

    add answers for lesson 4 - List

---
+++ exercise/3变量/chinese_zodiac.py
@@ -0,0 +1,3 @@
+Chinese_zodiac ='鼠牛虎兔龍蛇馬羊猴雞狗豬'
+
+Chinese_zodiac
---
+++ exercise/4序列/list.py
@@ -0,0 +1,47 @@
+# Ex 1, Strings
+
+char1 = ('Zhao Shujian')
+char2 = ("Let's Go")
+char3 = ('The Zen of Python')
+print(char1)
+print(char2)
+print(char3)
+print(char1,char2,char3)
+
+# Ex 2, String operation
+
+str1 = 'xyz'
+str2 = 'abc'
+str3 = str1 + str2
+print(str3)
+print(str1[1:2])
+print(str2*10)
+print('a' in str3)
+
+# Ex 3, list operation
+
+list1 = [1, 2, 3, 4, 5]
+list1.append(100)
+print(list1)
+
+list1.remove(2)
+print(list1)
+print(list1[0],list1[1], list1[2])
+print(list1[-1])
+print(list1[-2])
+
+# Ex 4, 元组
+
+tuple1 = (1 ,2, 3)
+# tuple1.append(12)
+print(tuple1[-2])
+
+tuple2 = (12, 13)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+print(len(tuple3))
+
+
+
+
+

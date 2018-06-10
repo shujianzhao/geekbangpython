f89be8e2069ce1232f440cc849090aa9054aaa49
Zhao Shujian <shujianzhao@gmail.com>
2018-06-10 12:34:04 +0800

    answers

---
+++ exercise/5 条件和循环/loop.py
@@ -0,0 +1,17 @@
+chinese_zodiac = '子鼠丑牛'
+
+for cz in chinese_zodiac:
+    print(cz)
+
+for i in range(1,13):
+    print(i)
+
+for year in range(2000,2019):
+    print('%s 年的生肖是' %(year))
+
+num = 5
+while True:
+    print(num)
+    num = num + 1
+    if num > 10:
+        break
